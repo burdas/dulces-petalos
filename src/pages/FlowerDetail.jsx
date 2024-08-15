1e32a6a11f6d88382c07f117712a79803e6449f8
burdas <burdasparmarcos@gmail.com>
2024-08-15 22:48:08 +0200

    Añadido el caso en el que la url no encuentre la flor

--- src/pages/FlowerDetail.jsx
+++ src/pages/FlowerDetail.jsx
@@ -1,30 +1,36 @@
 import Header from "../components/Header";
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import DetailView from "../components/Detailview";
 
-const BASE_URL = 'https://dulces-petalos.herokuapp.com/api/product/';
+const BASE_URL = "https://dulces-petalos.herokuapp.com/api/product/";
 function FlowerDetail() {
   const { id } = useParams();
   const [flower, setFlower] = useState(null);
 
   useEffect(() => {
-    fetch(`${BASE_URL}${id}`)// Obtenermos la respuesta de la peticion
-    .then((response) => response.json())
-    // Convertimos la respuesta a JSON
-    .then((data) => {
-      setFlower(data);
-    })
-    // Gestión de errores. De momento lo mostramos en consola.
-    .catch((error) => console.error(error));
-    }, [id]);
-    
-    return (
-      <>
-        <Header page={flower?.name} />
-        {flower && <DetailView props={flower} />}
-      </>
-    )
-  }
-  
-  export default FlowerDetail
+    fetch(`${BASE_URL}${id}`) // Obtenermos la respuesta de la peticion
+      .then((response) => response.json())
+      // Convertimos la respuesta a JSON
+      .then((data) => {
+        setFlower(data);
+      })
+      // Gestión de errores. De momento lo mostramos en consola.
+      .catch((error) => console.error(error));
+  }, [id]);
+
+  return (
+    <>
+      <Header page={flower?.name} />
+      {flower ? (
+        <DetailView props={flower} />
+      ) : (
+        <h1 className="text-4xl font-bold text-white text-center">
+          Flor no encontrada
+        </h1>
+      )}
+    </>
+  );
+}
+
+export default FlowerDetail;
