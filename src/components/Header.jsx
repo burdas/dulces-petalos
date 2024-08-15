47ca5851f062c459451a667e4ecb6a604778d04c
burdas <burdasparmarcos@gmail.com>
2024-08-15 22:40:39 +0200

    Arreglando estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Header(props) {
 
   return (
-    <header className="container mx-auto flex justify-left items-center p-4 flex-row gap-2">
+    <header className="container mx-auto flex justify-items-left items-center px-4 py-6 flex-row gap-2">
       <Link to="/">
         <h1 className="text-2xl font-bold text-white">Dulces Petalos</h1>
       </Link>
